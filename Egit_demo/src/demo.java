72d88d4a9fba70345dbfa99978e54a8eaf162c76
FA12-BSE-135 <FA12-BSE-135@LAB7C-16.cns.ciit>
2015-11-08 16:28:31 +0500

    new

--- Egit_demo/src/demo.java
+++ Egit_demo/src/demo.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class demo {
 	 public static void main(String args[])
 	   {
-		 System.out.print("go with flw of GIT");
+		 int a,b;
+		 Scanner scanner= new Scanner(System.in);
+		 a=scanner.nextInt();
+		 b=scanner.nextInt();
+		 int z=a+b;
+		 System.out.print("go with flw of GITand sum is "+ z);
 	   }
 
 }
